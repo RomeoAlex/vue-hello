1508fc4936d41e65aaaaae64b6ba30f630219201
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-02 16:51:50 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-// imprtare il debugger
-vue.config.devtools = true;
+// vue.config.devtools = true;
 // COME INIZIALIZZARE
-const app = new vue(
+const app = new Vue(
     {
         // per inizializzare ancorato al div con root
         el:'#root',
         data:{
-            titleText:'Ciao sono un Titolo VueJs!!',
-            subtitleText:'Benvenuto VUE',
-            // MAi VALORI VUOTI
-            valore:'',
-            // inizializzare o stringa vuota o false
-            valore: false
+            message:'Hello World',
+            image: 'https://mypuppies.net/wp-content/uploads/2021/08/q5-43.jpg'
         }
     }
 );
